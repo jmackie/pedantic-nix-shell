b90afb2243b7869eee747197fa746b1239ffde5a
Jordan Mackie <12185627+jmackie@users.noreply.github.com>
2024-04-12 12:49:29 +0100

    Add rust tooling

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
     pre-commit-hooks-nix.url = "github:cachix/pre-commit-hooks.nix";
     pre-commit-hooks-nix.inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
@@ -20,18 +21,33 @@
         inputs.treefmt-nix.flakeModule
         inputs.pre-commit-hooks-nix.flakeModule
       ];
-      perSystem = { pkgs, config, ... }:
+      perSystem = { pkgs, config, system, ... }:
+        let
+          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        in
         {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ (import inputs.rust-overlay) ];
+            config = { };
+          };
           treefmt.config = {
             inherit (config.flake-root) projectRootFile;
             package = pkgs.treefmt;
             programs = {
               nixpkgs-fmt.enable = true;
+              rustfmt.enable = true;
+              rustfmt.package = rustToolchain;
             };
           };
           pre-commit.settings.hooks = {
             treefmt.enable = true;
             treefmt.package = config.treefmt.build.wrapper;
+            clippy.enable = true;
+            clippy.packageOverrides.cargo = rustToolchain;
+            clippy.packageOverrides.clippy = rustToolchain;
+            clippy.settings.allFeatures = true;
+            clippy.settings.denyWarnings = true;
           };
           devShells.default =
             let
@@ -42,6 +58,8 @@
                 pkgs.fortune
                 pkgs.cowsay
                 pkgs.lolcat
+                rustToolchain
+                pkgs.rust-analyzer
               ] ++ treefmt-programs;
               welcomeShellHook = ''
                 fortune | cowsay | lolcat
