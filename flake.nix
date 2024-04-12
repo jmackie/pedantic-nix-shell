82fff989bda0e087bbf9ba0767d6d0949dd75241
Jordan Mackie <12185627+jmackie@users.noreply.github.com>
2024-04-12 12:27:41 +0100

    Add treefmt-nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,9 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-root.url = "github:srid/flake-root";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
@@ -11,16 +14,28 @@
         "x86_64-darwin"
       ];
       imports = [
+        inputs.flake-root.flakeModule
+        inputs.treefmt-nix.flakeModule
       ];
-      perSystem = { pkgs, ... }:
+      perSystem = { pkgs, config, ... }:
         {
+          treefmt.config = {
+            inherit (config.flake-root) projectRootFile;
+            package = pkgs.treefmt;
+            programs = {
+              nixpkgs-fmt.enable = true;
+            };
+          };
           devShells.default =
             let
+              treefmt = config.treefmt.build.wrapper;
+              treefmt-programs = builtins.attrValues config.treefmt.build.programs;
               tools = [
+                treefmt
                 pkgs.fortune
                 pkgs.cowsay
                 pkgs.lolcat
-              ];
+              ] ++ treefmt-programs;
               welcomeShellHook = ''
                 fortune | cowsay | lolcat
               '';
